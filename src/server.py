879d054687a2c02d5e685a957e829800cd8e4065
rendfall <rendfall@gmail.com>
2018-07-21 09:15:03 +0200

    Add first route + example data

--- src/server.py
+++ src/server.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, json, jsonify
 from flask import jsonify
 
 server = Flask(__name__)
 
-@server.route("/")
+@server.route('/')
 def index():
     return jsonify(
-        Hello="World",
-        Lorem="Ipsum"
+        Hello = 'World',
+        Lorem = 'Ipsum'
     )
 
+@server.route('/solar-system', methods = ['GET'])
+def get_solar_system():
+    # Source: https://raw.githubusercontent.com/duckduckgo/zeroclickinfo-goodies/master/share/goodie/cheat_sheets/json/solar-system.json
+    file = open('data/solar-system.json')
+    content = json.load(file)
+    return jsonify(content)
+
 if __name__ == '__main__':
-    #server.run(host='0.0.0.0', debug=True)
-    server.run(host='0.0.0.0')
+    server.run(host = '0.0.0.0', debug = True)
+    # server.run(host = '0.0.0.0')
