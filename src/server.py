989c579be6696d2eb1af0f210656a9e9f69795fb
rendfall <rendfall@gmail.com>
2018-07-28 23:14:36 +0200

    Add mongodb + add planet task

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
-from flask import Flask, json, jsonify
+from flask import Flask, json, jsonify, request
+from flask_pymongo import PyMongo
 
 server = Flask(__name__)
+
+mongo = PyMongo(server, uri = 'mongodb://localhost:27017/flask-api')
 
 @server.route('/')
 def index():
@@ -15,6 +18,18 @@ def get_solar_system():
     file = open('data/solar-system.json')
     content = json.load(file)
     return jsonify(content)
+
+@server.route('/planet/<name>', methods = ['GET'])
+def get_planet(name):
+    data = mongo.db.planets.find_one_or_404({ 'name': name })
+    return jsonify({ name: data['name'] })
+
+@server.route('/planet', methods = ['POST'])
+def add_planet():
+    name = request.json['name']
+    planet_id = mongo.db.planets.insert({ 'name': name })
+    new_planet = mongo.db.planets.find_one({ '_id': planet_id })
+    return jsonify({ 'name' : new_planet['name'] })
 
 if __name__ == '__main__':
     server.run(host = '0.0.0.0', debug = True)
